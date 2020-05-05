0a4663f2c761e8901df4616fc61a92d262fdd22d
Ryan McCue <me@ryanmccue.info>
2020-05-05 17:59:44 +0100

    Run pcov-clobber if installed

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 mysqld_safe --datadir=/var/lib/mysql &>/dev/null &
 sleep 1
+
+# Run https://github.com/krakjoe/pcov-clobber if installed.
+if [ -f /code/vendor/bin/pcov ]; then /code/vendor/bin/pcov clobber; fi
+
 /code/vendor/bin/phpunit "$@"
