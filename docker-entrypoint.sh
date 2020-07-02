1db7ec4e185768494cb4c427b2d7136a63237a8f
K Adam White <kadamwhite@users.noreply.github.com>
2020-07-02 15:47:56 -0400

    Adjust logic to poll until mysql returns non-error

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 mysqld_safe --datadir=/var/lib/mysql &>/dev/null &
 
-# Wait up to 5 seconds for mysql to start, checking once a second.
 retries=5
-until [ "$(pgrep mysql | wc -l)" <> 0 ] || [ $retries -le 0 ]; do
+status=1
+until [ $status -eq 0 ] || [ $retries -le 0 ]; do
+    mysql -uroot 2>/dev/null
+    status=$?
     retries=$((retries-1))
     sleep 1
 done
