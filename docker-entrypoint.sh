9e969a34efbe987141a26871d574e85710fc359f
K Adam White <kadamwhite@users.noreply.github.com>
2020-07-02 14:52:28 -0400

    Wait up to 5s for mysql, checking every second
    If MySQL takes >1s to start, which happens with some regularity in CI,
    the tests will not execute and a database connection error will be
    displayed in the test logs.
    
    Instead of "sleep 1," retry every second for 5s or until mysql starts.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 mysqld_safe --datadir=/var/lib/mysql &>/dev/null &
-sleep 1
+
+# Wait up to 5 seconds for mysql to start, checking once a second.
+retries=5
+until [ "$(pgrep mysql | wc -l)" <> 0 ] || [ $retries -le 0 ]; do
+    retries=$((retries-1))
+    sleep 1
+done
 
 # Run https://github.com/krakjoe/pcov-clobber if installed.
 if [ -f /code/vendor/bin/pcov ]; then /code/vendor/bin/pcov clobber; fi
